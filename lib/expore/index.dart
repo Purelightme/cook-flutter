432ef38bd030446140bc58df63586f1a4ccaece6
帅纯亮 <shuaichunliang@heqiauto.com>
2019-04-12 11:02:39 +0800

    sketch bottom bar

---
+++ lib/expore/index.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class ExploreIndex extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return new Center(
+      child: Text('explore_index'),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,152 +1,61 @@
-import 'package:easy_alert/easy_alert.dart';
 import 'package:flutter/material.dart';
-import 'package:english_words/english_words.dart';
-import 'package:startup_namer/examples/http.dart';
-import 'package:startup_namer/examples/jpush_demo.dart';
-import 'package:startup_namer/examples/layout_demo.dart';
-//import 'package:startup_namer/examples/base_widget.dart';
-import 'package:startup_namer/examples/navigator.dart';
-import 'package:startup_namer/examples/shared_preference_demo.dart';
-import 'package:startup_namer/examples/wechat.dart';
+import 'package:startup_namer/expore/index.dart';
+import 'package:startup_namer/index/index.dart';
+import 'package:startup_namer/my/index.dart';
 
 void main(){
-//  runApp(new AlertProvider(
-//    child: new MaterialApp(
-//      title: 'Flutter Tutorial',
-//      home: new TutorialHome(),
-//    ),
-//    config: new AlertConfig(
-//        ok: "OK text for ok button in AlertDialog",
-//        cancel: "CANCEL text for cancel button in AlertDialog"),
-//  ));
-
-//  runApp(new MaterialApp(
-//    title: 'Flutter Tutorial',
-//    home: new TutorialHome(),
-//  ));
-
-    runApp(new JpushDemo());
-
-//  runApp(new AlertProvider(
-//    child: new LayoutDemo(),
-//    config: new AlertConfig(
-//      ok: 'OK',
-//      cancel: 'Cancel'
-//    ),
-//  ));
-
+  runApp(new Cook());
 }
 
-//void main() => runApp(Center(child: new MyApp()));
-
-class MyApp extends StatelessWidget {
+class Cook extends StatefulWidget{
   @override
-  Widget build(BuildContext context) {
-    return new MaterialApp(
-      title: 'Startup Name Generator',
-      theme: new ThemeData(
-        primaryColor: Colors.redAccent,
-      ),
-      home: new RandomWords(),
-    );
+  State<StatefulWidget> createState() {
+    return new _cookState();
   }
+
 }
 
-class RandomWords extends StatefulWidget {
-  @override
-  createState() => new RandomWordsState();
-}
+class _cookState extends State<Cook>{
 
-class RandomWordsState extends State<RandomWords> {
+  int _currentIndex = 0;
+  final List<Widget> _children = [
+    new Index(),
+    new ExploreIndex(),
+    new MyIndex()
+  ];
+
+  void onTabTapped(int index) {
+    setState(() {
+      _currentIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
-      appBar: new AppBar(
-        title: new Text('Startup Name Generator'),
-        actions: <Widget>[
-          new IconButton(icon: new Icon(Icons.list), onPressed: _pushSaved),
+      appBar: AppBar(
+        title: Text('点外卖不如做菜')
+      ),
+      body: _children[_currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: onTabTapped,
+        currentIndex: 0,
+        items: [
+          BottomNavigationBarItem(
+            icon: new Icon(Icons.menu),
+            title: Text('菜谱')
+          ),
+          BottomNavigationBarItem(
+              icon: new Icon(Icons.search),
+              title: Text('发现')
+          ),
+          BottomNavigationBarItem(
+              icon: new Icon(Icons.person),
+              title: Text('我的')
+          ),
         ],
       ),
-      body: _buildSuggestions(),
     );
   }
 
-  void _pushSaved() {
-    Navigator.of(context).push(
-      new MaterialPageRoute(
-        builder: (context) {
-          final tiles = _saved.map(
-            (pair) {
-              return new ListTile(
-                title: new Text(
-                  pair.asPascalCase,
-                  style: _biggerFont,
-                ),
-              );
-            },
-          );
-          final divided = ListTile.divideTiles(
-            context: context,
-            tiles: tiles,
-          ).toList();
-          return new Scaffold(
-            appBar: new AppBar(
-              title: new Text('Saved Suggestions'),
-            ),
-            body: new ListView(children: divided),
-          );
-        },
-      ),
-    );
-  }
-
-  final _suggestions = <WordPair>[];
-  final _saved = new Set<WordPair>();
-  final _biggerFont = const TextStyle(fontSize: 18.0);
-
-  Widget _buildSuggestions() {
-    return new ListView.builder(
-        padding: const EdgeInsets.all(16.0),
-        // 对于每个建议的单词对都会调用一次itemBuilder，然后将单词对添加到ListTile行中
-        // 在偶数行，该函数会为单词对添加一个ListTile row.
-        // 在奇数行，该函数会添加一个分割线widget，来分隔相邻的词对。
-        // 注意，在小屏幕上，分割线看起来可能比较吃力。
-        itemBuilder: (context, i) {
-          // 在每一列之前，添加一个1像素高的分隔线widget
-          if (i.isOdd) return new Divider();
-
-          // 语法 "i ~/ 2" 表示i除以2，但返回值是整形（向下取整），比如i为：1, 2, 3, 4, 5
-          // 时，结果为0, 1, 1, 2, 2， 这可以计算出ListView中减去分隔线后的实际单词对数量
-          final index = i ~/ 2;
-          // 如果是建议列表中最后一个单词对
-          if (index >= _suggestions.length) {
-            // ...接着再生成10个单词对，然后添加到建议列表
-            _suggestions.addAll(generateWordPairs().take(10));
-          }
-          return _buildRow(_suggestions[index]);
-        });
-  }
-
-  Widget _buildRow(WordPair pair) {
-    final alreadySaved = _saved.contains(pair);
-    return new ListTile(
-      title: new Text(
-        pair.asPascalCase,
-        style: _biggerFont,
-      ),
-      trailing: new Icon(
-        alreadySaved ? Icons.favorite : Icons.favorite_border,
-        color: alreadySaved ? Colors.red : null,
-      ),
-      onTap: () {
-        setState(() {
-          if (alreadySaved) {
-            _saved.remove(pair);
-          } else {
-            _saved.add(pair);
-          }
-        });
-      },
-    );
-  }
 }
